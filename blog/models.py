be5fe3c55e9ffdd9125d03c16048a59996fec668
luopeng <peng.luo@idiaoyan.com>
2016-04-29 21:10:00 +0800

    create models and init enums

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 import mongoengine as models
+import datetime
+from blog import enums as blog_enums
 
 
 class Blog(models.Document):
-    create_time = models.IntField(default=None)
+    memebr_id = models.StringField()
+    create_time = models.DateTimeField(default=datetime.datetime.now)
+    update_time = models.DateTimeField(default=datetime.datetime.now)
     title = models.StringField()
+    content = models.StringField()
+    status = models.IntField(default=blog_enums.BLOG_STATUS_NORMAL)
+
+
+class Comment(models.Document):
+    blog_id = models.StringField()
+    create_time = models.DateTimeField(default=datetime.datetime.now)
+    content = models.StringField()
+    status = models.IntField(default=blog_enums.COMMENT_STATUS_NORMAL)
+    who = models.StringField()    # 由谁留下的评论, 可以是一个邮箱，手机或者ＱＱ号
+    ip = models.StringField()    # 发表评论的IP地址
